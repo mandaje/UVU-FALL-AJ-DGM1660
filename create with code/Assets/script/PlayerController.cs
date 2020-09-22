67918e96f7dfea78592deabaa266b7bad2a1df7d
Amanda <james9amanda@gmail.com>
2020-09-22 13:21:50 -0600

    Accidentally put this project on to the wrong place. code, and game

---
+++ create with code/Assets/script/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private int speed = 20;
+    private float turnSpeed = 100;
+
+    private float hInput;
+    private float fInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hInput = Input.GetAxis("Horizontal");
+        fInput = Input.GetAxis("Vertical");
+
+        // Move the vehicle forward base oof vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * fInput * speed);
+        //Rotated the vehicle left and right based on horizontal input
+        transform.Rotate(Vector3.up * Time.deltaTime * hInput * turnSpeed);
+    }
+}
