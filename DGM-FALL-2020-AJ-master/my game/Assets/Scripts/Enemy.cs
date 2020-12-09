7c7e365b065698a9d170eb10e9d51635817ff7d4
Amanda <james9amanda@gmail.com>
2020-12-09 00:49:16 -0700

    progress on game. no luck on enemy but prize is prime

---
+++ DGM-FALL-2020-AJ-master/my game/Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+public GameObject player;
+public bool gameOver = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void onCollisionEnter(Collision collision)
+       {   
+
+           if(collision.collider.gameObject == player)
+           {
+               gameObject.SetActive(true);
+               gameOver = true;               
+               Debug.Log("GAME OVER!");
+              
+           }
+       }
+}
--- DGM-FALL-2020-AJ-master/my game/Assets/Scripts/PlayerController.cs
+++ DGM-FALL-2020-AJ-master/my game/Assets/Scripts/PlayerController.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
  
  {
-    private int speed = 20;
+    private int speed = 30;
     private float turnSpeed = 100;
 
     private float hInput;
     private float fInput;
     public GameObject projectile;
 
+    public bool gameOver = false;
+    public bool hasPrize = false;
+    
+    public GameObject enemy;
+    private Rigidbody playerRb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -28,6 +33,27 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * fInput * speed);
         //Rotated the Player left and right based on horizontal input
         transform.Rotate(Vector3.up * Time.deltaTime * hInput * turnSpeed);
+    }
+    
+      private void OnTriggerEnter(Collider collider)
+    {
+      if(collider.CompareTag("Prize"))
+      {
+        hasPrize = true;
+        Debug.Log("Prize = " + hasPrize);
+        Destroy(collider.gameObject);
+      }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+      if(collision.gameObject.CompareTag("Enemy"))
+      {
+        Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+        gameOver = true;
+        Debug.Log(gameOver);
+        Destroy(collider.gameObject);
+      }
     }
 }
 
